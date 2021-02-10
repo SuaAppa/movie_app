65979dc43bfdd912ff27a3c49b2e7c1be595573d
SuaAppa <soundbrokaz@kakao.com>
2021-02-10 12:10:20 +0900

    last update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
-import {HashRouter,Route} from "react-router-dom";
+import {HashRouter,Route,BrowserRouter} from "react-router-dom";
 import About from "./routes/About";
 import Home from "./routes/Home";
+import "./App.css";
+import Navigation from "./components/Navigation"
 
 function App() {
     return (
         <HashRouter>
+            <Navigation />
             <Route path="/" exact={true} component={Home} />
             <Route path="/about" exact={true} component={About} />
         </HashRouter>
